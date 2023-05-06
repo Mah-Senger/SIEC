286bdbf30a7396d513449e00182116f6401f3ca6
Mah_senger <mariaeduardasenger1@gmail.com>
2023-05-05 21:04:42 -0300

    Arrumando a navbar de acordo com o protótipo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,128 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-header />
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img1.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption">
+          <h5>Lorem1 </h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <p><a href="h" class ="btn btn-light pt-3">Cadastrar</a></p>  
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img2.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption ">
+          <h5>Lorem 2</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <p><a href="h" class ="btn btn-light pt-3">Cadastrar</a></p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img3.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption ">
+          <h5>Lorem 3</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <p><a href="h" class ="btn btn-light pt-3">Cadastrar</a></p>
+         </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
-        <title>Laravel</title>
-
-    </head>
-    <body>
-        <h1>SIEC</h1>
-        <p>teste</p>
-    </body>
+  <!---parte dos objetivos-->
+ <section id="services" class="services section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <section-header class="section-header text-center pb-5">
+                    <h2>Nossos Objetivos</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                </section-header>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center bg-dark pb-2">
+                        <div class="crad-body">
+                            <i class="bi bi-1-circle"></i>
+                            <h3 class="crad-title"> Objetivo 1</h3>
+                            <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center bg-dark pb-2">
+                        <div class="crad-body">
+                            <i class="bi bi-1-circle"></i>
+                            <h3 class="crad-title"> Objetivo 2</h3>
+                            <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center bg-dark pb-2">
+                        <div class="crad-body">
+                            <i class="bi bi-1-circle"></i>
+                            <h3 class="crad-title"> Objetivo 3</h3>
+                            <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ </section>
+<!---sobre nós-->
+<section id="quemsomos" class="quemsomos sectio-paddin">
+    <div class="container">
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="card text-white text-center bg-dark pb-2">
+                <div class="crad-body">
+                    <i class="bi bi-1-circle"></i>
+                    <h3 class="crad-title"> Iara</h3>
+                    <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card text-white text-center bg-dark pb-2">
+                    <div class="crad-body">
+                        <i class="bi bi-1-circle"></i>
+                        <h3 class="crad-title">Larissa</h3>
+                        <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="card text-white text-center bg-dark pb-2">
+                        <div class="crad-body">
+                            <i class="bi bi-1-circle"></i>
+                            <h3 class="crad-title"> Maria </h3>
+                            <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card text-white text-center bg-dark pb-2">
+                            <div class="crad-body">
+                                <i class="bi bi-1-circle"></i>
+                                <h3 class="crad-title"> Milena</h3>
+                                <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+                            </div>
+                        </div>
+    </div>
+</section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
