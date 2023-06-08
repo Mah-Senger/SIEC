6fb48c9c5b0d5b526293dab9269d8df57bd83bdc
Mah_senger <mariaeduardasenger1@gmail.com>
2023-06-08 15:02:56 -0300

    Finalizando a página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
           <h6 class="font-weight-bold">Sistema de integração <br> empresa-candidato</h6>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          <p><a href="#"><button type="button" class="btn btn-primary btn-lg mt-4" style="background-color: #9AA7B2; border: none; color: #1B1B1B; width: 50%; font-style: bold; font-size: 1.4em; border: solid #89898C 1px;">Cadastre-se</button></a></p>
+          <p><a href="#"><button type="button" class="btn btn-primary btn-lg mt-4 btn-dark pt-3" style="background-color: #9AA7B2; border: none; color: #1B1B1B; width: 50%; font-style: bold; font-size: 1.4em; border: solid #89898C 1px;">CADASTRE-SE</button></a></p>
         </div>
       </div>
       <div class="carousel-item">
@@ -22,7 +22,7 @@
           <h5 class="font-weight-bold font-italic">Lorem 2</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          <p><a href="h" class ="btn btn-dark pt-3">Cadastrar</a></p>
+          <p><a href="#"><button type="button" class="btn btn-primary btn-lg mt-4 btn-dark pt-3" style="background-color: #9AA7B2; border: none; color: #1B1B1B; width: 50%; font-style: bold; font-size: 1.4em; border: solid #89898C 1px;">CADASTRE-SE</button></a></p>
         </div>
       </div>
       <div class="carousel-item">
@@ -31,7 +31,7 @@
           <h5>Lorem 3</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          <p><a href="h" class ="btn btn-dark pt-3">Cadastrar</a></p>
+         <p><a href="#"><button type="button" class="btn btn-primary btn-lg mt-4 btn-dark pt-3" style="background-color: #9AA7B2; border: none; color: #1B1B1B; width: 50%; font-style: bold; font-size: 1.4em; border: solid #89898C 1px;">CADASTRE-SE</button></a></p>
          </div>
       </div>
     </div>
@@ -89,42 +89,87 @@
  </section>
  
 <!---sobre nós-->
-<section id="quemsomos" class="quemsomos sectio-paddin">
-    <div class="container">
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="card text-white text-center bg-dark pb-2">
-                <div class="crad-body">
-                    <i class="bi bi-1-circle"></i>
-                    <h3 class="crad-title"> Iara</h3>
-                    <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <div class="card text-white text-center bg-dark pb-2">
-                    <div class="crad-body">
-                        <i class="bi bi-1-circle"></i>
-                        <h3 class="crad-title">Larissa</h3>
-                        <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-4">
-                    <div class="card text-white text-center bg-dark pb-2">
-                        <div class="crad-body">
-                            <i class="bi bi-1-circle"></i>
-                            <h3 class="crad-title"> Maria </h3>
-                            <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card text-white text-center bg-dark pb-2">
-                            <div class="crad-body">
-                                <i class="bi bi-1-circle"></i>
-                                <h3 class="crad-title"> Milena</h3>
-                                <p class="lead">adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
-                            </div>
-                        </div>
-    </div>
+<section id="time" class="team section-padding">
+   <div class="row">
+     <div class="section-header text-center pb-5">
+     <h2>Sobre Nós</h2>
+
+     </div>
+   </div>
+<div class="container d-flex justify-content-around">
+   <div class="row">
+     <div class="">
+        <div class="card text-center">
+          <div class="card-body">
+          <img src="" alt="" class="img-fluid rounded-circle">
+          <h3 class="card-title py-2">Iara Macedo</h3>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+
+          <p class="socials">
+            <i class="bi bi-linkedin text-dark mx-1"></i>
+            <i class="bi bi-github text-dark mx-1"></i>
+          </p>
+          </div>
+        </div>
+     </div>
+   </div>
+
+   <div class="row">
+     <div class="">
+        <div class="card text-center">
+          <div class="card-body">
+          <img src="" alt="" class="img-fluid rounded-circle">
+          <h3 class="card-title py-2">Larissa Padilha</h3>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+
+          <p class="socials">
+            <i class="bi bi-linkedin text-dark mx-1"></i>
+            <i class="bi bi-github text-dark mx-1"></i>
+          </p>
+          </div>
+        </div>
+     </div>
+   </div>
+
+
+   <div class="row">
+     <div class="">
+        <div class="card text-center">
+          <div class="card-body">
+          <img src="" alt="" class="img-fluid rounded-circle">
+          <h3 class="card-title py-2">Maria Senger</h3>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+
+          <p class="socials">
+            <i class="bi bi-linkedin text-dark mx-1"></i>
+            <i class="bi bi-github text-dark mx-1"></i>
+          </p>
+          </div>
+        </div>
+     </div>
+   </div>
+
+
+   <div class="row">
+     <div class="">
+        <div class="card text-center">
+          <div class="card-body">
+          <img src="" alt="" class="img-fluid rounded-circle">
+          <h3 class="card-title py-2">Milena Spina</h3>
+          <p class="card-text">Lorem ipsum dolor sit amet, consectetur <br> adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+
+          <p class="socials">
+            <i class="bi bi-linkedin text-dark mx-1"></i>
+            <i class="bi bi-github text-dark mx-1"></i>
+          </p>
+          </div>
+        </div>
+     </div>
+   </div>
+</div>
 </section>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 <x-footer />
 </body>
