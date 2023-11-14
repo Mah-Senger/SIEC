11803b756e48a0898b90849d427816b36014c7d0
LarissaCPadilha <larissa.padilha@aluno.ifsp.edu.br>
2023-11-14 16:42:33 -0300

    Traduzindo mensagem de erro cadastro candidato

--- app/Http/Controllers/CandidatoController.php
+++ app/Http/Controllers/CandidatoController.php
@@ -19,17 +19,34 @@ class CandidatoController extends Controller
     public function createCandidato(Request $request){
         $rules = [
             'nomeCandidato' => ['required', 'string', 'max:255'],
-            'emailCandidato' => ['required', 'string'],
-            'senhaCandidato' => ['string', 'required', 'min:8', 'max:20'],
-            'telefoneCandidato' => ['string', 'required'],
-            'cidadeCandidato' => ['required', 'string'],
             'cpfCandidato' => ['required', 'string', 'max:11', 'min:11'],
+            'emailCandidato' => ['required', 'string'],
+            'senhaCandidato' => [ 'required','string', 'min:8', 'max:20'],
+            'telefoneCandidato' => [ 'required','string'],
+            'cidadeCandidato' => ['required', 'string'],
             'experienciaCandidato' => ['required', 'string'],
             'idiomasCandidato' => ['required', 'string'],
             'formacaoCandidato' => ['required', 'string'],
             'formacaoDescricaoCandidato' => ['required', 'string']
         ];
-        $request->validate($rules);
+        $mensagem = [
+            'nomeCandidato.required'=>'Preencher o campo "Nome Completo" é obrigatório.',
+            'emailCandidato.required' => 'Preencher o campo "E-mail" é obrigatório.',
+            'senhaCandidato.required'=>'Preencher o campo "Senha" é obrigatório.',
+            'senhaCandidato.min'=>'O campo "Senha" precisa ter no mínimo 8 caracteres.',
+            'senhaCandidato.max'=>'O campo "Senha" tem o limite de 20 caracteres.',
+            'telefoneCandidato.required' =>'Preencher o campo "Telefone" é obrigatório.',
+            'cidadeCandidato.required' =>'Preencher o campo "Cidade" é obrigatório.',
+            'cpfCandidato.required' =>'Preencher o campo "CPF(Cadastro de Pessoa Física)" é obrigatório.',
+            'cpfCandidato.min' =>'O campo "CPF(Cadastro de Pessoa Física)" precisa ter 11 caracteres.',
+            'cpfCandidato.max' =>'O campo "CPF(Cadastro de Pessoa Física)" precisa ter 11 caracteres.',
+            'experienciaCandidato.required' =>'Preencher o campo "Experiência" é obrigatório.',
+            'idiomasCandidato.required' =>'Preencher o campo "Idiomas fluentes" é obrigatório.',
+            'formacaoCandidato.required' =>'Preencher o campo "Formação" é obrigatório.',
+            'formacaoDescricaoCandidato.required' => 'Preencher o campo "Descrição da Formação" é obrigatório.',
+            'confirmarSenhaCandidato.required'  => 'Preencher o campo "Confirmar senha" é obrigatório.'
+        ];
+        $request->validate($rules,$mensagem);
 
         if($request->senhaCandidato == $request->confirmarSenhaCandidato){
             $usuario = Usuarios::create([
