0950575500c3c444783078dff6147a583328907f
LarissaCPadilha <larissa.padilha@aluno.ifsp.edu.br>
2023-11-23 09:25:14 -0300

    Teste final

--- app/Http/Controllers/UserAuth.php
+++ app/Http/Controllers/UserAuth.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuarios;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,14 +17,29 @@ class UserAuth extends Controller{
             'email.required'=>'O E-mail é obrigatório.',
             'senha.required'=>'A Senha é obrigatória.'
         ]);
-        if(Auth::attempt(['email'=> $request->email, 'password' =>$request->password])){
-            return redirect()->route('dashboard');
-        }else{
-            return redirect()->back()->with('danger','E-mail ou Senha inválidos.');
+        $usuarios = Usuarios::where('email', '=', $request->email)->take(1)->get();
+
+            //     $usuario->email == $request->email;
+            //     $usuario->senha == $request->password;
+        
+        //foreach($usuarios as $usuario){
+            //dd($usuarios->nome);
+             //if(isset($usuarios->nome)){
+            
+                 if(Auth::attempt(['email'=> $request->email, 'password' =>$request->password])){
+                     return redirect()->route('dashboard');
+                 }else{
+                     return redirect()->back()->with('danger','E-mail ou Senha inválidos.');
+                 }
+            //     /*$data= $request->input();
+            //     $request->session()->put('email',$data['email']);
+            //     return redirect()->route('dashboard');*/
+            
+             //}else{
+               // return redirect()->back()->with('danger','E-mail ou Senha inválidos.');
+             //}
+        //}
+        
         }
-        /*$data= $request->input();
-        $request->session()->put('email',$data['email']);
-        return redirect()->route('dashboard');*/
-    }
 }
 ?>
