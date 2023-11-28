1a9627a58ba12e0ff82c07e4ffcfa995bba1c8e7
LarissaCPadilha <larissa.padilha@aluno.ifsp.edu.br>
2023-11-28 15:15:15 -0300

    CSS editar empresa

--- resources/views/empresa/editar.blade.php
+++ resources/views/empresa/editar.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>INICIO</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/pagina4.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&family=PT+Serif&display=swap" rel="stylesheet">
@@ -23,26 +24,42 @@
             </ul>
         </div>
     @endif
+    <div class="container">
+    <div class="title">Editar informações da Empresa</div><br>
+    <div class="content">
 
     <form action="{{ route('empresa.updateEmpresa') }}" method="post">
         @csrf
+    <div class="user-details">
+        <div class="input-box">
         <label for="nomeEmpresa">Nome da empresa: </label>
-        <input type="text" id="nomeEmpresa" class="" name="nomeEmpresa" value="{{ $usuariosEmpresa['nome'] }}"><br>
+        <input type="text" id="nomeEmpresa" class="" name="nomeEmpresa" value="{{ $usuariosEmpresa['nome'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="cnpjEmpresa">CNPJ (Cadastro Nacional de Pessoa Jurídica): </label>
-        <input type="text" id="cnpjEmpresa" class="" name="cnpjEmpresa" value="{{ $usuariosEmpresa['cnpj'] }}"><br>
+        <input type="text" id="cnpjEmpresa" class="" name="cnpjEmpresa" value="{{ $usuariosEmpresa['cnpj'] }}">
+        </div>
         
+        <div class="input-box">
         <input type="hidden" id="emailEmpresa" class="" name="emailEmpresa" value="{{ $usuariosEmpresa['nome'] }}">
 
+        <div class="input-box">
         <label for="senhaEmpresa">Senha: </label>
-        <input type="password" id="senhaEmpresa" class="" name="senhaEmpresa"><br>
+        <input type="password" id="senhaEmpresa" class="" name="senhaEmpresa">
+        </div>
 
+        <div class="input-box">
         <label for="confirmarSenhaEmpresa">Confirmar senha: </label>
-        <input type="password" id="confirmarSenhaEmpresa" class="" name="confirmarSenhaEmpresa"><br>
+        <input type="password" id="confirmarSenhaEmpresa" class="" name="confirmarSenhaEmpresa">
+        </div>
 
+        <div class="input-box">
         <label for="telefoneEmpresa">Telefone: </label>
-        <input type="text" id="telefoneEmpresa" class="" name="telefoneEmpresa" value="{{ $usuariosEmpresa['telefone'] }}"><br>
+        <input type="text" id="telefoneEmpresa" class="" name="telefoneEmpresa" value="{{ $usuariosEmpresa['telefone'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="cidadeEmpresa">Cidade que a empresa se localiza: </label>
         <select name="cidadeEmpresa" value="{{ $usuariosEmpresa['cidade'] }}">
             <option value="Alambari">Alambari-SP</option>
@@ -56,23 +73,37 @@
             <option value="São Miguel Arcanjo">São Miguel Arcanjo-SP</option>
             <option value="Sarapuí">Sarapuí-SP</option>
             <option value="Tatuí">Tatuí-SP</option>
-        </select><br>
+        </select>
+        </div>
 
+        <div class="input-box">
         <label for="ruaEndEmpresa">Rua: </label>
-        <input type="text" id="ruaEndEmpresa" class="" name="ruaEndEmpresa" value="{{ $usuariosEmpresa['ruaEnd'] }}"><br>
+        <input type="text" id="ruaEndEmpresa" class="" name="ruaEndEmpresa" value="{{ $usuariosEmpresa['ruaEnd'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="numEndEmpresa">Número: </label>
-        <input type="number" id="numEndEmpresa" class="" name="numEndEmpresa" value="{{ $usuariosEmpresa['numEnd'] }}"><br>
+        <input type="number" id="numEndEmpresa" class="" name="numEndEmpresa" value="{{ $usuariosEmpresa['numEnd'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="bairroEndEmpresa">Bairro: </label>
-        <input type="text" id="bairroEndEmpresa" class="" name="bairroEndEmpresa" value="{{ $usuariosEmpresa['bairroEnd'] }}"><br>
+        <input type="text" id="bairroEndEmpresa" class="" name="bairroEndEmpresa" value="{{ $usuariosEmpresa['bairroEnd'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="cepEndEmpresa">CEP: </label>
-        <input type="text" id="cepEndEmpresa" class="" name="cepEndEmpresa" value="{{ $usuariosEmpresa['cepEnd'] }}"><br>
+        <input type="text" id="cepEndEmpresa" class="" name="cepEndEmpresa" value="{{ $usuariosEmpresa['cepEnd'] }}">
+        </div>
 
+        <div class="input-box">
         <label for="objHistEmpresa">Objetivos e história da empresa: </label>
-        <textarea name="objHistEmpresa" id="objHistEmpresa" value="">{{ $usuariosEmpresa['objHistEmpresa'] }}</textarea><br>
+        <textarea name="objHistEmpresa" id="objHistEmpresa" value="">{{ $usuariosEmpresa['objHistEmpresa'] }}</textarea>
+        </div>
 
-        <button type="submit">Cadastrar</button>
-    </form><br>
+        <button type="submit" class="button">Editar</button><br><br><br>
+    </form><br><br><br><br>
+    </div>
+    </div>
+    </div>
 <x-footer />
